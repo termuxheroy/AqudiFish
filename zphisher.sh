cbe956eb1aabd3b096740ea9dd2872d095d10d93
termuxheroy <144564892+termuxheroy@users.noreply.github.com>
2024-03-02 13:25:59 +0300

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -203,27 +203,26 @@ check_status() {
 ## Banner
 banner() {
 	cat <<- EOF
-		${ORANGE}
-		${ORANGE} ______      _     _     _               
-		${ORANGE}|___  /     | |   (_)   | |              
-		${ORANGE}   / / _ __ | |__  _ ___| |__   ___ _ __ 
-		${ORANGE}  / / | '_ \| '_ \| / __| '_ \ / _ \ '__|
-		${ORANGE} / /__| |_) | | | | \__ \ | | |  __/ |   
-		${ORANGE}/_____| .__/|_| |_|_|___/_| |_|\___|_|   
-		${ORANGE}      | |                                
-		${ORANGE}      |_|                ${RED}Version : ${__version__}
+  ${ORANGE}              _______              _____    ________    
+  ${ORANGE}   / /\ \    |  ___  |   | |    | || __ \   |_______|   
+  ${ORANGE}  / /  \ \   | |   | |   | |    | || |  \ \    | |
+  ${ORANGE} / /    \ \  | |   | |   | |    | || |   | |   | |     
+  ${ORANGE}/ /______\ \ | |   | |   | |    | || |   | |   | |    
+  ${ORANGE}| |______| | | |   | |   | |    | || |   / /   | |     
+  ${ORANGE}| |      | | | |___| |   \ \___/ / | |__/ /   _|_|___  
+  ${ORANGE}| |      | | |_______|-|  \_____/  |_____/  |________|  Tools    ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Aqudi (tahmid.rayat)${WHITE}
 	EOF
 }
 
 ## Small Banner
 banner_small() {
 	cat <<- EOF
-		${BLUE}
-		${BLUE}  ░▀▀█░█▀█░█░█░▀█▀░█▀▀░█░█░█▀▀░█▀▄
-		${BLUE}  ░▄▀░░█▀▀░█▀█░░█░░▀▀█░█▀█░█▀▀░█▀▄
-		${BLUE}  ░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀${WHITE} ${__version__}
+		${BLUE} Aqudi
+		${BLUE} vk.com/aqudi 
+		${BLUE} discord - Aqudix
+		${BLUE} <3 ${WHITE} ${__version__}
 	EOF
 }
 
